4b18eb619fb31b5ee0b13a1651fba4aafb27b800
MadhubabuNulakani <madhubabu935.mm@gmail.com>
2024-10-28 17:07:32 +0530

    update with latest code

--- shopmate/src/components/ProductsList.js
+++ shopmate/src/components/ProductsList.js
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react'
 
 const ProductsList = () => {
     const [products, setProducts] = useState([]);
-
-
+    if (products.length > 0)
+        console.log(products)
     useEffect(() => {
         fetch("http://localhost:8000/Products")
             .then(response => response.json())
             .then(data => setProducts(data));
-    },[])
+    }, [])
 
 
-    console.log(products);
 
     return (
         <div>
-            Products List
+            {products.map((product) => (
+                <p>{product.name}</p>
+            ))}
         </div>
     )
 }
